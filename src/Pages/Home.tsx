4bf8a80745d78245eff56db52c70813c53208a81
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2024-01-04 02:33:14 +0100

    liet

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -10,6 +10,7 @@ import {GiHamburgerMenu} from "react-icons/gi";
 const Home = () => {
     const [showHeader2, setShowHeader2] = useState(false);
     const [smallScreen, setSmallScreen] = useState(false);
+    const [dropNav, setDropNav] = useState(false);
 
     const handleScroll = () => {
         const scrollPosition = window.scrollY;
@@ -30,6 +31,10 @@ const Home = () => {
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
+
+    const handleDropNav = () => {
+        setDropNav(!dropNav);
+    };
     return (
         <>
             <div className="w-full h-max">
@@ -38,11 +43,11 @@ const Home = () => {
                     <div
                         className={`${
                             smallScreen
-                                ? "w-full h-20 bg-transparent px-28 border-b-[1px] border-b-gray-400"
-                                : "w-full h-20 bg-[#7b577b] bg-gradient-to-r from-orange-400 via-transparent to-indigo-700 px-28 phone:px-0 fixed top-0 left-0 z-20"
+                                ? "w-full h-max bg-transparent transition-all duration-500  px-28 border-b-[1px] border-b-gray-400"
+                                : "w-full h-max bg-[#7b577b] transition-all duration-500  bg-gradient-to-r from-orange-400 via-transparent to-indigo-700 px-28 phone:px-0 fixed top-0 left-0 z-20 "
                         }`}
                     >
-                        <div className="w-full flex justify-between items-center">
+                        <div className="w-full flex justify-between h-20 items-center">
                             <div className="w-max h-full">
                                 <img src={logo} alt="" className="w-56 h-20" />
                             </div>
@@ -71,13 +76,47 @@ const Home = () => {
                                 </NavLink>
                             </div>
                             <div className="w-max h-max phone:flex hidden pr-5">
-                                <GiHamburgerMenu className="w-8 h-8 cursor-pointer text-white" />
+                                <GiHamburgerMenu
+                                    className="w-8 h-8 cursor-pointer text-white"
+                                    onClick={handleDropNav}
+                                />
                             </div>
                         </div>
-                        <div
-                            className={`w-full h-[21rem] bg-black hidden phone:flex`}
-                        >
-                            kdk
+                        <div className="w-full h-max">
+                            <div
+                                className={`w-full pb-2 ${
+                                    dropNav
+                                        ? "max-h-[23rem] overflow-hidden transition-all duration-500"
+                                        : "max-h-0 overflow-hidden transition-all duration-500 "
+                                } `}
+                            >
+                                <div
+                                    className={`w-full h-max pl-5 flex flex-col gap-4 pt-2`}
+                                >
+                                    <ul className="w-max h-full flex flex-col gap-8 text-white text-xl font-semibold ">
+                                        <li className="w-max h-full flex items-center transition-all cursor-pointer hover:border-b-2 hover:border-b-white">
+                                            Home
+                                        </li>
+                                        <li className="w-max h-full flex items-center transition-all cursor-pointer hover:border-b-2 hover:border-b-white">
+                                            About Us
+                                        </li>
+                                        <li className="w-max h-full flex items-center transition-all cursor-pointer hover:border-b-2 hover:border-b-white">
+                                            Plans
+                                        </li>
+                                        <li className="w-max h-full flex items-center transition-all cursor-pointer hover:border-b-2 hover:border-b-white">
+                                            Contact
+                                        </li>
+                                        <li className="w-max h-full flex items-center transition-all cursor-pointer hover:border-b-2 hover:border-b-white">
+                                            FAQ
+                                        </li>
+                                    </ul>
+                                    <NavLink to={"/register"}>
+                                        <button className="px-8 py-3 bg-gradient-to-r from-[#903eff] to-indigo-900 h-max w-max rounded-full text-white">
+                                            Start Now !
+                                        </button>
+                                    </NavLink>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="w-full h-[120vh] flex">
