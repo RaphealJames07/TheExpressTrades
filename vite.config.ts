b58c7d2220966c43265712f83a7e314bc9c3298d
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2024-02-04 20:49:11 +0100

    production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/https://theexpresstrades.com/', // Set your base URL if hosting in a subdirectory
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory for the production build
+    assetsDir: '', // Set to an empty string to place assets directly in outDir
+    sourcemap: false, // Disable sourcemaps in production
+  },
+});
